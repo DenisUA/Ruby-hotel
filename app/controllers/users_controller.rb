7550dfdb79ea2c6e9762ea3564e455f81af060ec
Yaroslav <liakhyaroslav@gmail.com>
2017-05-15 20:51:09 +0300

    Added format.json and remove comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,16 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
-  # GET /users
   def index
     @user = User.all
   end
 
-  # GET /users/1
   def show; end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
     respond_to do |format|
@@ -22,38 +18,40 @@ class UsersController < ApplicationController
         format.html do
           redirect_to @user, notice: 'User was successfully created.'
         end
+        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # GET /users/1/edit_user
   def edit; end
 
-  # PATCH/PUT /users/1
   def update
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user }
+        format.json { render json: @user }
       else
         format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
 private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :phone, :admin, :gender, :date_of_birth)
+    params.require(:user).permit(:first_name, :last_name, :email, :phone, :admin, :gender, :birth_date)
   end
 
   def set_user
