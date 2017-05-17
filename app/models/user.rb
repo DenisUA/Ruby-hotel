592da4fec792e3d1a6c325cf861094dd61276d05
Yaroslav <liakhyaroslav@gmail.com>
2017-05-17 15:24:53 +0300

    Hotfix for user.rb
    Remove '!' from messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  validates :first_name, :last_name, presence: { message: 'Please, fill the field!' },
+  validates :first_name, :last_name, presence: true,
                                      format: { with: /\A[A-Z].*\z/, message: 'Please, start from upper case' }
   validates :email, uniqueness: true,
                     format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i },
-                    presence: { message: 'Please, fill the field with correct email!' }
+                    presence: { message: 'Please, fill the field with correct email' }
   validates :phone, uniqueness: true,
-                    presence: { message: 'Please, fill the field with correct phone number!' }
+                    presence: { message: 'Please, fill the field with correct phone number' }
 
   def full_name
     "#{last_name} #{first_name}"
