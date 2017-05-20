fb623242d7e642375771391af639557ca9701d38
VitaliyMaznyi <vitaliymaznuy@gmail.com>
2017-05-20 18:23:09 +0300

    rubocop fix
    fix code style and add config/initializers/simple_form_bootstrap.rb to ignore for rubocop

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,3 @@
 class RegistrationsController < Devise::RegistrationsController
   respond_to :json
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}, controllers: { registrations: 'registrations' }
+  devise_for :users, path: '',
+                     path_names: { sign_in: 'login', sign_out: 'logout' },
+                     controllers: { registrations: 'registrations' }
   resources :users
   resources :apartments
 
