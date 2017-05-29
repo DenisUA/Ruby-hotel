0de3319f5320e5a556a2edb53362521e1770f923
Yaroslav <liakhyaroslav@gmail.com>
2017-05-29 23:30:39 +0300

    Create logik for filter in homes controller

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,23 @@
 class HomesController < ApplicationController
   def index
+    sql_arr_of_params = ['price >= :price_from',
+                         'price <= :price_to',
+                         'room_type = :room_type',
+                         'occupancy = :occupancy']
+    query = 'status = 0 AND' + sql_arr_of_params.join(' AND ')
+    @apartment_filter = Apartment
+                            .select_all
+                            .where(query,
+                                   {price_from: params[:price_from],
+                                    price_to: params[:price_to],
+                                    room_type: params[:room_type],
+                                    occupancy: params[:occupancy]})
+                            .to_hash
+  end
+
+  private
+
+  def apartment_params
+    params.require(:apartment_filter).permit(:status, :price, :description, :room_type, :occupancy)
   end
 end
