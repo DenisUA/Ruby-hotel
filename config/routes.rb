5e3a82b768a324d384c0487dfbc2d998d97b3e16
Yaroslav <liakhyaroslav@gmail.com>
2017-05-29 21:31:19 +0300

    Initial commit for home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users, path: '',
                      path_names: { sign_in: 'login', sign_out: 'logout' },
                      controllers: { registrations: 'registrations' }
   resources :users
   resources :apartments
   resources :orders
+  resources :home, only: :index
 
   root 'apartments#index'
 end
