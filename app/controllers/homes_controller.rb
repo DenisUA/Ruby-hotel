858f7eddbbaf45effcc3257f4d2462c7f9ae67c8
Yaroslav <liakhyaroslav@gmail.com>
2017-05-29 23:43:37 +0300

    Correct logik for filter, without date

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,14 +4,18 @@ class HomesController < ApplicationController
                          'price <= :price_to',
                          'room_type = :room_type',
                          'occupancy = :occupancy']
-    query = 'status = 0 AND' + sql_arr_of_params.join(' AND ')
+    query = 'status = 0'
+    query + ' AND ' + sql_arr_of_params[0] unless params[:price_from].nil?
+    query + ' AND ' + sql_arr_of_params[1] unless params[:price_to].nil?
+    query + ' AND ' + sql_arr_of_params[2] unless params[:room_type].nil?
+    query + ' AND ' + sql_arr_of_params[3] unless params[:occupancy].nil?
+
+    params_hash = {}
+    params.each_key {|key| params_hash << key}
     @apartment_filter = Apartment
                             .select_all
                             .where(query,
-                                   {price_from: params[:price_from],
-                                    price_to: params[:price_to],
-                                    room_type: params[:room_type],
-                                    occupancy: params[:occupancy]})
+                                   params_hash)
                             .to_hash
   end
 
