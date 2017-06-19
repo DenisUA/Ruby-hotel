b5f67ad3cafd7254167a5c1b3e67f181eb4ccc9b
Yaroslav <liakhyaroslav@gmail.com>
2017-06-19 11:20:31 +0300

    Rebuild schema.rb and Gemfile.lock to resolve error with bundle

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522181655) do
+ActiveRecord::Schema.define(version: 20170522222733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
