eb4f3eaebdeb560073b660d9684f4483dc05010b
Yaroslav <liakhyaroslav@gmail.com>
2017-06-19 10:04:24 +0300

    Added Controller and Form Object for room filter

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,13 +1,12 @@
 require 'ApartmentsFilterForm.rb'
 class HomesController < ApplicationController
-
   def index
     @apartments_filter = ApartmentsFilterForm.new if params[:apartments_filter_form].nil?
     @apartments_filter = ApartmentsFilterForm.new(filter_params) unless params[:apartments_filter_form].nil?
     @apartments = ApartmentsFilter.new(filter_params).filter
   end
 
-  private
+private
 
   def filter_params
     unless params[:apartments_filter_form].nil?
--- app/models/ApartmentsFilterForm.rb
+++ app/models/ApartmentsFilterForm.rb
@@ -9,11 +9,21 @@ class ApartmentsFilterForm
       :end_at
   )
 
-  # validate :start_from, date => Date.today
-  #                                                message: "must be at least #{Date.today}" }, on: :index
-  # validates :end_at, date: { after_or_equal_to: :start_from }
-  # validates :price.to_s.split(', ')[0], numericality: { greater_than: 0 } unless :price.nil?
-  # validates :price.to_s.split(', '), numericality: { greater_than: 0 } unless :price.nil?
-  # validates :occupancy, numericality: { greater_than: 0, less_than: 11 }
+  validate :start_from_date_not_in_past
+  validate :price_from
+  validates :occupancy, numericality: { greater_than: 0, less_than: 11 }
+
+  private
+
+  def start_from_date_not_in_past
+    return unless start_from < Date.today
+    errors.add :start_from, 'start date cannot be in past'
+  end
+
+  def price_from
+    price = :price
+    return unless price.split(', ')[0] < 0
+    errors.add :price, 'price cannot be less than 0'
+  end
 
 end
