6a93ec6454197f38e662686f49b1f8b0bb51c3e1
Yaroslav <liakhyaroslav@gmail.com>
2017-06-27 00:05:34 +0300

    Refactor order controller
    Removed creating pdf from services and created this services

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,12 +13,13 @@ class OrdersController < ApplicationController
   def edit
     return unless @order.cancelled? && !current_user.admin?
     respond_to do |format|
-      format.html { redirect_to @order }
-      format.json { render json: @order.errors, status: :forbidden }
+      format.html {redirect_to @order}
+      format.json {render json: @order.errors, status: :forbidden}
     end
   end
 
-  def show; end
+  def show;
+  end
 
   def create
     @order = Order.new(order_params)
@@ -26,11 +27,12 @@ class OrdersController < ApplicationController
     @order.total = count_price
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created' }
-        format.json { render :show, status: :created, location: @order }
+        OrderMailer.send_invoice(@order, CreatePdf.new(@order).make_pdf_from_html).deliver
+        format.html {redirect_to @order, notice: 'Order was successfully created'}
+        format.json {render :show, status: :created, location: @order}
       else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @order.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -38,11 +40,11 @@ class OrdersController < ApplicationController
   def update
     respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to @order }
-        format.json { render json: @order }
+        format.html {redirect_to @order}
+        format.json {render json: @order}
       else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @order.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -51,18 +53,18 @@ class OrdersController < ApplicationController
     @order.cancelled!
     if @order.cancelled?
       respond_to do |format|
-        format.html { redirect_to orders_path, notice: 'Order was cancelled' }
-        format.json { head :ok }
+        format.html {redirect_to orders_path, notice: 'Order was cancelled'}
+        format.json {head :ok}
       end
     else
       respond_to do |format|
-        format.html { redirect_to orders_path, notice: 'Order wasn\'t cancelled due to error' }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.html {redirect_to orders_path, notice: 'Order wasn\'t cancelled due to error'}
+        format.json {render json: @order.errors, status: :unprocessable_entity}
       end
     end
   end
 
-private
+  private
 
   def order_params
     params.require(:order).permit(:start_from, :end_at, :total, :apartment_id)
@@ -75,4 +77,5 @@ private
   def count_price
     ((@order[:end_at].to_time - @order[:start_from].to_time)/86400) * @order[:total]
   end
+
 end
---
+++ app/services/create_pdf.rb
@@ -0,0 +1,17 @@
+class CreatePdf
+  def initialize(order)
+    @order = order
+  end
+
+  def make_pdf_from_html
+    WickedPdf.new.pdf_from_string(
+        ActionController::Base.new.render_to_string(
+            :template => 'templates/invoice.pdf.slim',
+            :locals => {
+                :order => @order
+            }
+        ),
+        :pdf => 'invoice',
+        :layout => 'pdf.slim')
+  end
+end
