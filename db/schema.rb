73a14723a5e589d7528260127984b07872bd8027
VitaliyMaznyi <vitaliymaznuy@gmail.com>
2017-06-30 19:43:34 +0300

    add stripe setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531171904) do
+ActiveRecord::Schema.define(version: 20170626173025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170531171904) do
     t.integer "occupancy", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "photos"
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -32,6 +33,46 @@ ActiveRecord::Schema.define(version: 20170531171904) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "oauth_access_grants", force: :cascade do |t|
+    t.integer "resource_owner_id", null: false
+    t.bigint "application_id", null: false
+    t.string "token", null: false
+    t.integer "expires_in", null: false
+    t.text "redirect_uri", null: false
+    t.datetime "created_at", null: false
+    t.datetime "revoked_at"
+    t.string "scopes"
+    t.index ["application_id"], name: "index_oauth_access_grants_on_application_id"
+    t.index ["token"], name: "index_oauth_access_grants_on_token", unique: true
+  end
+
+  create_table "oauth_access_tokens", force: :cascade do |t|
+    t.integer "resource_owner_id"
+    t.bigint "application_id"
+    t.string "token", null: false
+    t.string "refresh_token"
+    t.integer "expires_in"
+    t.datetime "revoked_at"
+    t.datetime "created_at", null: false
+    t.string "scopes"
+    t.string "previous_refresh_token", default: "", null: false
+    t.index ["application_id"], name: "index_oauth_access_tokens_on_application_id"
+    t.index ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true
+    t.index ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id"
+    t.index ["token"], name: "index_oauth_access_tokens_on_token", unique: true
+  end
+
+  create_table "oauth_applications", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "uid", null: false
+    t.string "secret", null: false
+    t.text "redirect_uri", null: false
+    t.string "scopes", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
   end
 
   create_table "orders", force: :cascade do |t|
@@ -45,6 +86,14 @@ ActiveRecord::Schema.define(version: 20170531171904) do
     t.bigint "apartment_id"
     t.index ["apartment_id"], name: "index_orders_on_apartment_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.bigint "apartment_id"
+    t.string "pic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["apartment_id"], name: "index_photos_on_apartment_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -68,10 +117,16 @@ ActiveRecord::Schema.define(version: 20170531171904) do
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
     t.datetime "remember_created_at"
+    t.string "stripe_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
+  add_foreign_key "oauth_access_grants", "users", column: "resource_owner_id"
+  add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
+  add_foreign_key "oauth_access_tokens", "users", column: "resource_owner_id"
   add_foreign_key "orders", "apartments"
   add_foreign_key "orders", "users"
+  add_foreign_key "photos", "apartments"
 end
